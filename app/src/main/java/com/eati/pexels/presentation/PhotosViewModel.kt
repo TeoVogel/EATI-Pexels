00c8d5012333985595a38397c30158edc02fa352
Teo Vogel <teovogel97@gmail.com>
2023-03-03 11:09:43 -0300

    Fix bug empty query not working

--- app/src/main/java/com/eati/pexels/presentation/PhotosViewModel.kt
+++ app/src/main/java/com/eati/pexels/presentation/PhotosViewModel.kt
@@ -3,17 +3,21 @@ package com.eati.pexels.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.eati.pexels.data.PhotosRepository
-import com.eati.pexels.domain.Photo
+import com.eati.pexels.domain.PhotoExt
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class PhotosViewModel(private val repository: PhotosRepository) : ViewModel() {
 
-    val photosFlow = MutableStateFlow<List<Photo>>(listOf())
+    val photosFlow = MutableStateFlow<List<PhotoExt>>(listOf())
 
     fun updateResults(query: String) {
         viewModelScope.launch {
-            val results = repository.getPhotos(query)
+            val results = if (query.isBlank())
+                emptyList<PhotoExt>()
+            else
+                repository.getPhotos(query)
+
             photosFlow.emit(results)
         }
     }
